b86cb2161ff08379b8957a58ab53910c5d6b6876
ksaumaz <64628051+ksaumaz@users.noreply.github.com>
2022-02-16 20:41:10 -0600

    Ignore database

--- config/database.js
+++ config/database.js
@@ -1,18 +1,13 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-	client: 'postgres',
-	connection: {
-	  host: config.host,
-	  port: config.port,
-	  database: config.database,
-	  user: config.user,
-	  password: config.password,
-	  ssl: {
-		rejectUnauthorized: false
-	  },
-	},
-	debug: false,
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'ec2-54-235-98-1.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd517t5dqdnd56v'),
+      user: env('DATABASE_USERNAME', 'hkghawwlshtcaf'),
+      password: env('DATABASE_PASSWORD', '6dc847185b372110908ffd12ca93abdee13d93150d74978c9dbcf72f76b47da5'),
+	  ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)},
+    },
   },
 });
